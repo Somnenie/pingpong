11a26bdf75de2196636ee848bf269ea17e5e793e
Somnenie <somnenier@mail.ru>
2025-04-19 17:31:16 +0300

    Add files via upload

--- game_ping-pong.py
+++ game_ping-pong.py
@@ -1,4 +1,12 @@
 from pygame import *
+from random import *
+from typing import Any 
+
+win_heidth = 455
+win_wight = 802
+
+dx = 3
+dy = 3
 
 win = display.set_mode((802, 455))
 background = transform.scale(image.load('Board.png'),(802, 455))
@@ -6,6 +14,53 @@ display.set_caption('Ping pong for Nikito4ka')
 
 clock = time.Clock()
 FPS = 60
+
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, says_x, says_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (says_x, says_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys [K_w] and self.rect.y >5:
+            self.rect.y -= self.speed
+        if keys [K_s] and self.rect.y < win_heidth - 150 :
+            self.rect.y += self.speed
+
+class Friend(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys [K_UP] and self.rect.y >5:
+            self.rect.y -= self.speed
+        if keys [K_DOWN] and self.rect.y < win_heidth - 150:
+            self.rect.y += self.speed
+
+class Enemy(GameSprite):
+    def update(self):
+        global popusk
+        
+        self.rect.x += self.speed
+
+        if self.rect.y > win_heidth:
+            self.rect.x = randint(80, win_wight - 80)
+            self.rect.y = 0
+            popusk = popusk + 1
+        if ball.rect.colliderect(friend.rect):
+            dx *= -1
+        
+
+
+player = Player('Computer.png', 80, 180, 18, 120, 10)
+friend = Friend('Player.png', 700, 180, 18, 120, 10)
+ball = Enemy('Ball.png', 400, 180, 45, 45, 10)
 
 finish = False
 game = True 
@@ -18,7 +73,20 @@ while game:
     if finish != True:
         win.blit(background,(0, 0))
 
+       
+
+    player.reset()
+    player.update()
+    friend.reset()
+    friend.update()
+    ball.reset()
+    ball.update()
+
+ 
+
+     
+    
     display.update()
     clock.tick(FPS)
 
-    print('ni')
+    
